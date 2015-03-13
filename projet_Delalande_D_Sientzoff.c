10e6c7fc7129431dd51f2c2ad6084cace69bf135
TheoDel <theo.deldel@gmail.com>
2015-03-13 15:25:56 +0000

    Ajout matrice creuse

--- projet_Delalande_D_Sientzoff.c
+++ projet_Delalande_D_Sientzoff.c
@@ -10,14 +10,25 @@
 #include <glpk.h>
 #include "shiny_parcer.h"
 
-/* déclaration du prototype de la fonction */
-void shiny_reader( char* filename, donnees *dat );
-
 int main( int argc, char *argv[] )
 {
+	/*Initialisation des données du problème*/
+	donnees donprob;
+
 	/*Nombre de variables x_ij*/
 	int nbvar;
 	/*Nombre de contraintes de base (seulement (1) et (2)*/
 	int nbcont;
+
+	/*Initialisation des données et de la matrice creuse pour GLPK */
+	glp_prob *prob;
+	int *ia;
+	int *ja;
+	double *ar;
+
+	/*Nombre de coeff non-nuls dans la matrice de contraintes*/
+	int nbcreux;
+
+	nbcreux = 2*donprob.n*donprob.n;
 }
 
