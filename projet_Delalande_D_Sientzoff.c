55b25fa0c75c3ded6ac3f76484088ca190ae3c44
TheoDel <theo.deldel@gmail.com>
2015-03-13 15:02:44 +0000

    Ajout structure des données

--- projet_Delalande_D_Sientzoff.c
+++ projet_Delalande_D_Sientzoff.c
@@ -1,5 +1,5 @@
 /**
- ** Théo <met ton nom ici>
+ ** Théo DELALANDE-DELARBRE
  ** Benjamin SIENTZOFF
  ** 601A
  ** Shiny fleet
@@ -8,3 +8,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <glpk.h>
+
+typedef struct {
+	int n; /*Nombre de lieux à visiter n*/
+	int *d; /*Matrice des distances c_ij*/
+	int nbvar; /*Nombre de variables x_ij*/
+	int nbcont; /*Nombre de contraintes de base (seulement (1) et (2)*/
+} donnees;
+
+
+int main(int argc, char *argv[])
+{
+	
+}
